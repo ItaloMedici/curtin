7fbaae9ee3f291e6f0ad1900c202586ecf9f6227
ItaloMedici <59889993+ItaloMedici@users.noreply.github.com>
2024-06-02 19:42:23 -0300

    feat: add providers and imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+import { useCases } from '@application/use-cases';
+import { UrlController } from '@infrastructure/controller/url.controller';
+import { repositoriesProvider } from '@infrastructure/providers/respository';
+import { schemas } from '@infrastructure/schemas';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature(schemas),
+    ConfigModule.forRoot(),
+  ],
+  controllers: [UrlController],
+  providers: [...repositoriesProvider, ...useCases],
 })
 export class AppModule {}
